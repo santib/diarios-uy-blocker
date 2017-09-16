fee3754b4160797c208760f893dd298b9a539bc3
Santiago Bartesaghi <sbartesaghi@wyeworks.com>
2017-09-16 14:58:52 -0300

    Fix when the script is called from ovaciondigital

--- request-blocker.js
+++ request-blocker.js
@@ -1,3 +1,3 @@
 chrome.webRequest.onBeforeRequest.addListener(function() {
   return { cancel: true };
-}, { urls: ["*://www.elpais.com.uy/js/epd_sw*"] }, ["blocking"] );
+}, { urls: ["*://*/js/epd_sw*"] }, ["blocking"] );
